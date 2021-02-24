63a3089e38d937b0f8ec34821270c92186297764
Linielson <linielsonrosa@gmail.com.br>
2021-02-24 07:36:56 -0300

    Using Mux router

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
 )
@@ -17,47 +18,35 @@ type Post struct {
 var db, err = sql.Open("mysql", "root:12345678@/go_course?charset=utf8")
 
 func main() {
-	//Insert register
-	//stmt, err := db.Prepare("Insert into posts(title, body) values(?,?)")
-	//checkError(err)
-	//
-	//_, err = stmt.Exec("My First Post", "My first content")
-	//checkError(err)
-	//db.Close()
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
 
-	rows, error := db.Query("select * from posts")
-	checkError(error)
-
-	var items []Post
-
-	for rows.Next() {
-		//var id int
-		//var title string
-		//var body string
-		//rows.Scan(&id, &title, &body)
-		//fmt.Println(id, title, body)
-
-		post := Post{}
-		rows.Scan(&post.Id, &post.Title, &post.Body)
-		items = append(items, post)
-	}
-
-	db.Close()
-
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		t := template.Must(template.ParseFiles("templates/index.html"))
-		if err := t.ExecuteTemplate(writer, "index.html", items); err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	fmt.Println(http.ListenAndServe(":8080", nil))
-	//localhost:8080
-	//127.0.0.1:8080
+	fmt.Println(http.ListenAndServe(":8080", r))
 }
 
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func HomeHandler(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("templates/index.html"))
+	if err := t.ExecuteTemplate(writer, "index.html", ListPosts()); err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func ListPosts() []Post {
+	rows, error := db.Query("select * from posts")
+	checkError(error)
+
+	var items []Post
+
+	for rows.Next() {
+		post := Post{}
+		rows.Scan(&post.Id, &post.Title, &post.Body)
+		items = append(items, post)
+	}
+	return items
 }
